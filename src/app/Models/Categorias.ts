8eeaa9ff321adf4d78820768530cfa5270329055
Guillermo Gabard Delgado <you@example.com>
2022-05-18 16:38:52 +0200

    Agrego listado de categorías

---
+++ src/app/Models/Categorias.ts
@@ -0,0 +1,35 @@
+export interface Categoria {
+  nombreCategoria: string;
+  clave: string;
+}
+
+export const categorias: Categoria[] = [
+  {
+    nombreCategoria: "Pantalones",
+    clave: "1"
+  },
+  {
+    nombreCategoria: "Faldas",
+    clave: "2"
+  },
+  {
+    nombreCategoria: "Camisas",
+    clave: "3"
+  },
+  {
+    nombreCategoria: "Abrigos",
+    clave: "4"
+  },
+  {
+    nombreCategoria: "Vestidos",
+    clave: "5"
+  },
+  {
+    nombreCategoria: "Zapatos",
+    clave: "6"
+  },
+  {
+    nombreCategoria: "Gorras",
+    clave: "7"
+  }
+];
--- src/app/Pages/add-product/add-product.component.ts
+++ src/app/Pages/add-product/add-product.component.ts
@@ -1,9 +1,10 @@
+import { Categoria, categorias } from './../../Models/Categorias';
 import { CambioPaginaService } from './../../Services/CambioPagina/cambio-pagina.service';
 import { Product } from './../../Models/product.interface';
 import { HttpClientModule } from '@angular/common/http';
-
 import { LoadingService } from './../../Services/Loading/loading.service';
 import { Component, OnInit, } from '@angular/core';
+
 
 
 
@@ -23,8 +24,14 @@ export class AddProductComponent implements OnInit {
   }
 
   principal: number = 1;//Variable que se envía para el cambio de página.
+  lista: Categoria[];
+  nombreProducto: string= undefined;
+  categoriaProducto: string=undefined;
+  descripcionProducto: string = undefined;
+  precioCompra:string= undefined;
+  precioVenta: string=undefined;
+  imagenProducto: string;
 
-  descripcion: String = undefined;
 
   constructor(
     private loadingService: LoadingService,
@@ -33,7 +40,21 @@ export class AddProductComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.listaCategorias();
+    setTimeout(() => {
+      console.log(this.lista);
+    }, 4500);
   }
+
+
+
+  public onChangeCategoria(){
+    console.log("hola")
+
+  }
+  public listaCategorias() {
+    this.lista = categorias;
+  };
 
   public atras() {
     this.cambio.cambioPagina(this.principal);
